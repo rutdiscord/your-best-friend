5850794903782097f26f4d2cbdeacedd930a9ef6
Orangestar <orangestar@cats4gold.net>
2021-04-26 00:02:29 -0500

    More DP exception checks

--- ybf/__init__.py
+++ ybf/__init__.py
@@ -73,6 +73,114 @@ class Client(discord.Client):
         for exception in settings.purge['ignored_content']:
             if exception in message.content:
                 return True
+        
+        # box drawing characters
+        if re.findall("/(?:[\u2500-\u25FF\u2800-\u28FF]\s*){4,}/", message.content):
+            return True
+        
+        # wingdings
+        exceptions = {
+            # zodiac
+            '\u264B\uFE0E',
+            '\u264C\uFE0E',
+            '\u264D\uFE0E',
+            '\u264E\uFE0E',
+            '\u264F\uFE0E',
+            '\u2650\uFE0E',
+            '\u2651\uFE0E',
+            '\u2652\uFE0E',
+            '\u2653\uFE0E',
+            '\u264A\uFE0E',
+            '\u2648\uFE0E',
+            '\u2649\uFE0E',
+
+            '\U0001F670', # SCRIPT LIGATURE ET ORNAMENT
+            '\U0001F675', # Swash Ampersand Ornament
+            '\u25CF\uFE0E', # Black Circle
+            '\u274D\uFE0E', # Shadowed White Circle
+            '\u25A0\uFE0E', # BLACK SQUARE FOR STOP
+            '\u25A1\uFE0E', # White Square
+            '\u25FB\uFE0E', # White Medium Square
+            '\u2751\uFE0E', # Lower Right Shadowed Square
+            '\u2752\uFE0E', # Upper Right Shadowed Square
+            '\u2B27\uFE0E', # Black Medium Lozenge
+            '\u29EB\uFE0E', # Black Lozenge
+            '\u25C6\uFE0E', # Black Diamond 
+            '\u2756\uFE0E', # Black Diamond Minus White X
+            '\u2B25\uFE0E', # Black Medium Diamond
+            '\u2327\uFE0E', # X In A Rectangle Box
+            '\u2353\uFE0E', # APL Functional Symbol Quad Up Caret
+            '\u2318\uFE0E', # Place of Interest Sign
+            '\u270C\uFE0E', # V for Victory 
+            '\U0001F44C\uFE0E', # OK Hand
+            '\U0001F44D\uFE0E', # Thumbs Up Sign
+            '\U0001F44E\uFE0E', # Thumbs Down Sign
+            '\u261C\uFE0E', # 'White Left Pointing Index
+            '\u261E\uFE0E', # White Right Pointing Index
+            '\u261D\uFE0E', # White Up Pointing Index
+            '\u261F\uFE0E', # White Down Pointing Index
+            '\u270B\uFE0E', # Raised Hand Emoji
+            '\u263A\uFE0E', # Smiling Face
+            '\U0001F610\uFE0E', # Neutral Face
+            '\u2639\uFE0E', # White Frowning Face
+            '\U0001F4A3\uFE0E', # Bomb
+            '\u2620\uFE0E', # Skull and Crossbones
+            '\u2690\uFE0E', # White Flag
+            '\U0001F3F1\uFE0E', # White Pennant
+            '\u2708\uFE0E', # Airplane
+            '\u263C\uFE0E', # White Sun with Rays
+            '\U0001F4A7\uFE0E', # Droplet
+            '\u2744\uFE0E', # Snowflake
+            '\U0001F546\uFE0E', # White Latin Cross
+            '\u271E\uFE0E', # Shadowed White Latin Cross
+            '\U0001F548\uFE0E', # Celtic Cross
+            '\u2720\uFE0E', # Maltese Cross
+            '\u2721\uFE0E', # Star of David
+            '\u262A\uFE0E', # Star and Crescent
+            '\U0001F4C2\uFE0E', # Open File Folder
+            '\U0001F4C4\uFE0E', # Page Facing Up
+            '\U0001F5CF\uFE0E', # Page
+            '\U0001F5D0\uFE0E', # Pages
+            '\U0001F5C4\uFE0E', # File Cabinet
+            '\u231B\uFE0E', # Hourglass
+            '\U0001F5AE\uFE0E', # Wired Keyboard
+            '\U0001F5B0\uFE0E', # Two Button Mouse
+            '\U0001F5B2\uFE0E', # Trackball
+            '\U0001F4C1\uFE0E', # File Folder
+            '\u275E\uFE0E', # Heavy Double Comma Quotation Mark Ornament
+            '\u275D\uFE0E', # Heavy Double Turned Comma Quotation Mark Ornament
+            '\u270F\uFE0E', # Pencil
+            '\u200B', # Zero-width space
+            '\u2701\uFE0E', # Upper Blade Scissors
+            '\U0001F453\uFE0E', # Eyeglasses
+            '\U0001F56D\uFE0E', # Ringing Bell
+            '\U0001F56E\uFE0E', # Open Book
+            '\U0001F582\uFE0E', # Back of Envelope
+            '\U0001F57F\uFE0E', # Black Telephone
+            '\u2706\uFE0E', # Black Touchtone Telephone
+            '\U0001F4EB\uFE0E', # Closed Mailbox with Raised Flag
+            '\U0001F5AC\uFE0E', # Soft Shell Floppy Disk
+            '\U0001F583\uFE0E', # Stamped Envelope
+            '\U0001F5AB\uFE0E', # White Hard Shell Floppy Disk
+            '\u2707\uFE0E', # Tape Drive
+            '\U0001F4EA\uFE0E', # Closed Mailbox with Lowered Flag
+            '\U0001F4EC\uFE0E', # Open Mailbox with Raised Flag
+            '\U0001F4ED\uFE0E', # Open Mailbox with Lowered Flag
+            '\u270D\uFE0E', # Writing Hand Emoji
+            '\U0001F5B4\uFE0E', # Hard Disk
+            '\U0001F5B3\uFE0E', # Old Personal Computer
+            '\U0001F56F\uFE0E', # Candle
+            '\u2702\uFE0E', # Black Scissors
+            '\u262F\uFE0E', # Yin Yang
+            '\u2740\uFE0E', # White Florette
+            '\u2638\uFE0E', # Wheel of Dharma
+            '\u0950\uFE0E', # Devanagari Om
+            '\u273F\uFE0E', # Black Florette
+            '\U0001F58E', # Left Writing Hand
+        }
+
+        if any(item in exceptions for item in message.content):
+            return True
 
     #### EVENTS ####
 
@@ -117,9 +225,6 @@ class Client(discord.Client):
                 ]['rolebanned'] in message.author.roles # ignore rolebanned users
         ):
             await self.check_for_mentions(message)
-            banned_msg = await self.check_for_banned_messages(message)
-            if(banned_msg):
-                await message.delete()
             return
             # TODO: This exact code shows up thrice. Maybe squish it all into one func?
 
@@ -136,9 +241,6 @@ class Client(discord.Client):
         if not invocation: # not a command
             if not direct_message: # message in this server
                 await self.check_for_mentions(message)
-                banned_msg = await self.check_for_banned_messages(message)
-                if(banned_msg):
-                    await message.delete()
                     
             return # don't continue to check for a command
 
@@ -152,9 +254,6 @@ class Client(discord.Client):
             return
 
         await self.check_for_mentions(message)
-        banned_msg = await self.check_for_banned_messages(message)
-        if(banned_msg):
-            await message.delete()
 
     async def on_member_ban(self, guild, user):
         settings.purge['ignored_users'].append(user.id)
