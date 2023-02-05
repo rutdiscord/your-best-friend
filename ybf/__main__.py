ca719cc14fe22368cecdf5bd8324d5e5db3478e6
Daniel "Orangestar" O <orangestar@cats4gold.net>
2023-02-05 00:17:44 -0600

    explicitly request all intents
    it's a single-server mod bot, screw it

--- ybf/__main__.py
+++ ybf/__main__.py
@@ -1,8 +1,9 @@
+import disnake
 import ybf # import base necessities
 
 print('Starting up...')
 
-YBF = ybf.Client()
+YBF = ybf.Client(intents=disnake.Intents.all())
 
 KEY = None
 with open('./ybf/configs/token.key', encoding='utf-8') as keyfile:
